a5fe42a2860653f7237a98a8a335551bd7ba9e14
Lizzy <l.grootjen@student.ru.nl>
2021-11-18 11:27:44 +0100

    kleine update met stdp

--- SNN/SNNFPGA.py
+++ SNN/SNNFPGA.py
@@ -2,18 +2,26 @@ import nengo
 import numpy as np
 
 from nengo_fpga.networks import FpgaPesEnsembleNetwork
-## Testing if everything works
-
-def input_func(t):
-    return [np.sin(t * 2*np.pi), np.cos(t * 2*np.pi)]
 
 with nengo.Network() as model:
 
     # Input stimulus
-    input_node = nengo.Node(input_func)
+    input_node_left = nengo.Node(0)
+    input_node_right = nengo.Node(0)
 
-    # "Pre" ensemble of neurons, and connection from the input
-    ens_fpga = FpgaPesEnsembleNetwork('de1', n_neurons=50,
-                                      dimensions=2,
-                                      learning_rate=1e-4)
-    nengo.Connection(input_node, ens_fpga.input)  # Note the added '.input'
+    input_layer = nengo.Ensemble(n_neurons=100, dimensions=2)
+
+    nengo.Connection(input_node_left, input_layer[0])
+    nengo.Connection(input_node_right, input_layer[1])
+
+
+    hidden_layer = nengo.Ensemble(n_neurons=256, dimensions=2)
+    nengo.Connection(input_layer, hidden_layer)
+
+
+    output_layer1 = nengo.Ensemble(n_neurons=25, dimensions=1)
+    output_layer2 = nengo.Ensemble(n_neurons=25, dimensions=1)
+
+    nengo.Connection(hidden_layer[0], output_layer1)
+    nengo.Connection(hidden_layer[1], output_layer2)
+
