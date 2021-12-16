4770f0affe64bc6146f8bf21318bf7dca0aa9622
Fendor, Z. (Zuzanna) <z.fendor@student.ru.nl>
2021-12-16 10:16:50 +0100

    Beginning of the small example in dummylearning

--- SNN/DummyLearning.py
+++ SNN/DummyLearning.py
@@ -25,42 +25,54 @@ def plot_decoded(t, data, xlim_tuple=None):
 
     plt.show()
 
+def error_func(desired_L, desired_R, actual_L, actual_R):
+    left = np.abs(desired_L - actual_L)
+    right = np.abs(desired_R - actual_R)
+    return np.mean(left + right)
 
 with nengo.Network(label="STDP") as model:
     noise_period = 60
     cutoff_freq = 5
-    stim_left_t = nengo.Node(WhiteSignal(noise_period, high=cutoff_freq), size_out=1)
-    stim_left_o = nengo.Node(WhiteSignal(noise_period, high=cutoff_freq), size_out=1)
-    stim_right_t = nengo.Node(WhiteSignal(noise_period, high=cutoff_freq), size_out=1)
-    stim_right_o = nengo.Node(WhiteSignal(noise_period, high=cutoff_freq), size_out=1)
+    my_spikes_L = [[0,1,0,0,0,0,1,1],[0,1,0,0,0,0,1,1]]
+    input_node_L = nengo.Node(nengo.processes.PresentInput(my_spikes_L, 0.001))
+    my_spikes_R = [[0, 1, 0, 0, 1, 0, 1, 1], [1, 1, 0, 0, 1, 0, 1, 1]]
+    input_node_R = nengo.Node(nengo.processes.PresentInput(my_spikes_R, 0.001))
+    # https://forum.nengo.ai/t/spike-train-input-to-a-snn-model/717/4
 
-    inp_collector_l = nengo.Ensemble(1,dimensions = 1)
-    inp_collector_r = nengo.Ensemble(1,dimensions = 1)
+    # stim_left_t = nengo.Node(output=1 , size_out=1)
+    # stim_left_o = nengo.Node(output =1, size_out=1)
+    # stim_right_t = nengo.Node(output = 1, size_out=1)
+    # stim_right_o = nengo.Node(output = 1, size_out=1)
+
+    inp_collector_l = nengo.Ensemble(8, dimensions = 8)
+    inp_collector_r = nengo.Ensemble(8, dimensions = 8)
 
 
     #input
-    input_a = nengo.Ensemble(1,dimensions = 1)
-    input_b = nengo.Ensemble(1,dimensions = 1)
+    input_a = nengo.Ensemble(8,dimensions = 8)
+    input_b = nengo.Ensemble(8,dimensions = 8)
 
     #hidden
-    hidden_a = nengo.Ensemble(1, dimensions = 1)
-    hidden_b = nengo.Ensemble(1, dimensions = 1)
-    hidden_c = nengo.Ensemble(1, dimensions = 1)
+    hidden_a = nengo.Ensemble(8, dimensions = 8)
+    hidden_b = nengo.Ensemble(8, dimensions = 8)
+    hidden_c = nengo.Ensemble(8, dimensions = 8)
 
     #output
-    output_a = nengo.Ensemble(1, dimensions = 1)
-    output_b = nengo.Ensemble(1, dimensions = 1)
+    output_a = nengo.Ensemble(8, dimensions = 8)
+    output_b = nengo.Ensemble(8, dimensions = 8)
 
     outa_p = nengo.Probe(output_a)
     outb_p = nengo.Probe(output_b)
 
     ## connections  between the layers
+    Dij = 0.01
 
-    nengo.Connection(stim_left_t, inp_collector_l)
-    nengo.Connection(stim_left_o, inp_collector_l)
 
-    nengo.Connection(stim_right_t, inp_collector_r)
-    nengo.Connection(stim_right_o, inp_collector_r)
+
+    nengo.Connection(input_node_L, inp_collector_l, synapse =0.05 - Dij)
+
+    nengo.Connection(input_node_R, inp_collector_r, synapse = 0.05 + Dij)
+
 
     nengo.Connection(inp_collector_l, input_a, solver=nengo.solvers.LstsqL2(weights=True),
         learning_rule_type=stdp.STDP(learning_rate=2e-9))
@@ -113,8 +125,11 @@ with nengo.Network(label="STDP") as model:
 
 
 with nengo.Simulator(model) as sim:
-    sim.run(3.0)
+    sim.run(1.0)
 t = sim.trange()
 
+
+print(sim.data[outa_p][0])
+print(sim.data[outa_p][1])
 
 plot_decoded(t, sim.data)
