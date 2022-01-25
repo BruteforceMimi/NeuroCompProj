c3fe06aec84f11819affcba2f6aa40432c77787e
Fendor, Z. (Zuzanna) <z.fendor@student.ru.nl>
2022-01-25 16:01:14 +0100

    almost a skeleton of the learning algorithm

--- SNN/DummyLearning.py
+++ SNN/DummyLearning.py
@@ -60,43 +60,35 @@ def read_data():
 
     return left, right, target_L, target_R
 
-def transform_to_train(model, pre_neurons, post_neurons):
+def transform_to_train(model, pre_neuron, post_neuron):
     with model:
         Dij = 0.001
+        to_remove = []
         for conn in model.all_connections:
-            # disconnect from sensory input
-            if (conn.pre_obj is inp_collector_l or conn.pre_obj is inp_collector_r) and conn.transform != 0.0:
-                #add connection to pre neurons
-                if conn.post_obj in post_neurons:
-                    nengo.Connection(train_signal_generator, conn.post_obj, synapse = 0.005+Dij)
-                    model.connections.remove(conn)
-                if conn.post_obj in pre_neurons:
-                    nengo.Connection(train_signal_generator, conn.post_obj, synapse=0.005)
-                    model.connections.remove(conn)
-                else:
-                    nengo.Connection(conn.pre_obj, conn.post_obj, transform = 0.0)
-                    model.connections.remove(conn)
+            if conn.post_obj is inp_collector_lgoal or conn.post_obj is inp_collector_lter or conn.post_obj is inp_collector_rgoal or               conn.post_obj is inp_collector_rter:
+                to_remove.append(conn)
+        for conn2 in to_remove:
+            model.connections.remove(conn2)
 
-            # change connections to STDP
-            elif isinstance(conn.pre_obj, nengo.Ensemble) and conn.learning_rule_type is None:
-                nengo.Connection(conn.pre_obj, conn.post_obj, solver = nengo.solvers.LstsqL2(weights=True),learning_rule_type = stdp.STDP(learning_rate=2e-9))
-                model.connections.remove(conn)
-
-
-        # connect chosen training pair to training signal generating nodes
-        # (make sure that one trained neuron gets the signal with a delay of Dij
-        # I don't know which value that should be so for now I just guess something reasonable)
-
-
+        pre_train_sig_conn = nengo.Connection(train_signal_generator, pre_neuron, synapse = 0.005)
+        post_train_sig_conn = nengo.Connection(train_signal_generator, post_neuron, synapse = 0.005+Dij)
     return model
 
 def transform_to_validate(model):
     with model:
+        #remove all trainging connections to input collectors
+        to_remove = []
+        for conn in model.all_connections:
+            if conn.post_obj is inp_collector_lgoal or conn.post_obj is inp_collector_lter or conn.post_obj is inp_collector_rgoal or conn.post_obj is inp_collector_rter:
+                to_remove.append(conn)
+        for conn2 in to_remove:
+            model.connections.remove(conn2)
 
-        #change the connections to non-learning connections
-        #reconnect with the sensory input
-        #disconnect from the train signal generator
-        pass
+        #add the sensor connections back to where they belong
+        nengo.Connection(input_node_L[0], inp_collector_lter)
+        nengo.Connection(input_node_L[1], inp_collector_lgoal)
+        nengo.Connection(input_node_R[0], inp_collector_rter)
+        nengo.Connection(input_node_R[1], inp_collector_rgoal)
 
 with nengo.Network(label="STDP") as model:
     timing = 0.1
@@ -116,9 +108,10 @@ with nengo.Network(label="STDP") as model:
     # https://forum.nengo.ai/t/spike-train-input-to-a-snn-model/717/4
     input_node_probe = nengo.Probe(input_node_L)
 
-    inp_collector_l = nengo.Ensemble(2, dimensions=2)
-    inp_collector_r = nengo.Ensemble(2, dimensions=2)
-
+    inp_collector_lter = nengo.Ensemble(1, dimensions=1)
+    inp_collector_rter = nengo.Ensemble(1, dimensions=1)
+    inp_collector_lgoal = nengo.Ensemble(1, dimensions=1)
+    inp_collector_rgoal = nengo.Ensemble(1, dimensions=1)
     # input
     input_a = nengo.Ensemble(1, dimensions=1)
     input_b = nengo.Ensemble(1, dimensions=1)
@@ -138,17 +131,18 @@ with nengo.Network(label="STDP") as model:
     outb_p = nengo.Probe(output_b)
     ## connections  between the layers
 
-    nengo.Connection(input_node_L, inp_collector_l)
+    nengo.Connection(input_node_L[0], inp_collector_lter)
+    nengo.Connection(input_node_L[1], inp_collector_lgoal)
+    nengo.Connection(input_node_R[0], inp_collector_rter)
+    nengo.Connection(input_node_R[1], inp_collector_rgoal)
 
-    nengo.Connection(input_node_R, inp_collector_r)
-
-    nengo.Connection(inp_collector_l[0], input_a,  solver=nengo.solvers.LstsqL2(weights=True),
+    nengo.Connection(inp_collector_lter, input_a,  solver=nengo.solvers.LstsqL2(weights=True),
                      learning_rule_type=stdp.STDP(learning_rate=2e-9))
-    nengo.Connection(inp_collector_r[0], input_b, solver=nengo.solvers.LstsqL2(weights=True),
+    nengo.Connection(inp_collector_rter, input_b, solver=nengo.solvers.LstsqL2(weights=True),
                      learning_rule_type=stdp.STDP(learning_rate=2e-9))
-    nengo.Connection(inp_collector_l[1], input_c, solver=nengo.solvers.LstsqL2(weights=True),
+    nengo.Connection(inp_collector_lgoal, input_c, solver=nengo.solvers.LstsqL2(weights=True),
                      learning_rule_type=stdp.STDP(learning_rate=2e-9))
-    nengo.Connection(inp_collector_r[1], input_d, solver=nengo.solvers.LstsqL2(weights=True),
+    nengo.Connection(inp_collector_rgoal, input_d, solver=nengo.solvers.LstsqL2(weights=True),
                      learning_rule_type=stdp.STDP(learning_rate=2e-9))
 
     nengo.Connection(input_a, hidden_a, solver=nengo.solvers.LstsqL2(weights=True),
@@ -198,36 +192,13 @@ with nengo.Network(label="STDP") as model:
     nengo.Connection(hidden_a, hidden_c)
 
 
-    # ## WIP: calculate error through ensembles
-    # target_L = nengo.Node(nengo.processes.PresentInput(target_freq_L, timing))
-    # target_R = nengo.Node(nengo.processes.PresentInput(target_freq_R, timing))
-    # error_L = nengo.Ensemble(10, dimensions = 1)
-    # error_R = nengo.Ensemble(10, dimensions = 1)
-    #
-    # nengo.Connection(output_a, error_L, synapse=None, transform = -1)
-    # nengo.Connection(target_L, error_L, synapse=None, transform = -1)
-    #
-    # nengo.Connection(output_b, error_R, synapse=None, transform = -1)
-    # nengo.Connection(target_R, error_R, synapse=None, transform = -1)
-    #
-    # # -- inhibit errors after 40 seconds - gestolen van de tutorial
-    # inhib = nengo.Node(lambda t: 2.0 if t > 40.0 else 0.0)
-    # nengo.Connection(inhib, error_L.neurons, transform=[[-1]] * error_L.n_neurons)
-    # nengo.Connection(inhib, error_R.neurons, transform=[[-1]] * error_R.n_neurons)
-    #
-    # # -- probes
-    # target_p_L = nengo.Probe(target_L, synapse=0.01)
-    # pre_p_L = nengo.Probe(input_node_L, synapse=0.01)
-    # post_L_p = nengo.Probe(output_a, synapse=0.01)
-    # error_L_p = nengo.Probe(error_L, synapse=0.03)
-
 duration = 0.1
 error = 10
 error_limit  = 1
 pre_neurons = [input_a]
 post_neurons = [input_c]
-i = 0
-while i< 1:
+index = 0
+while index < 10:
     with nengo.Simulator(model) as sim:
         sim.run(duration)
         freq_a = np.sum(sim.data[outa_p] > 0, axis=0) / len(sim.data[outa_p])
@@ -237,25 +208,30 @@ while i< 1:
     # and how it relates to the target freqs
     # optionnaly, the output should be transformed somehow
     new_error = error_func(target_freq_L, target_freq_R, sim.data[outa_p], sim.data[outb_p])#
-
+    print(error)
     sim.data.reset()
 
     if new_error <= error:
         model = transform_to_train(model, pre_neurons, post_neurons)
     else:
-        #reverse the training signal.
-        #TODO: make the pairs separately instead of grouping them like that
         i = pre_neurons.copy()
         pre_neurons = post_neurons.copy()
         post_neurons = i
-        model = transform_to_train(model, pre_neurons, post_neurons)
+        for pre_neuron, post_neuron in zip(pre_neurons, post_neurons):
+            model = transform_to_train(model, pre_neuron, post_neuron)
+            with nengo.Simulator(model) as sim:
+                sim.run(0.4)
+
+    error = new_error
 
 
     with nengo.Simulator(model) as sim:
         sim.run(duration)
         freq_a = np.sum(sim.data[outa_p] > 0, axis=0) / len(sim.data[outa_p])
         freq_b = np.sum(sim.data[outb_p] > 0, axis=0) / len(sim.data[outb_p])
-        i+=1
+        index +=1
+
+
 t = sim.trange()
 print("freq A", freq_a)
 print("freq B", freq_b)
