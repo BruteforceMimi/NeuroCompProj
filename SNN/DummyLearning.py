fa03e7e6a853d8af9002109915925ad2008ea5bc
Fendor, Z. (Zuzanna) <z.fendor@student.ru.nl>
2022-01-26 16:35:52 +0100

    error function in progress

--- SNN/DummyLearning.py
+++ SNN/DummyLearning.py
@@ -6,16 +6,6 @@ import csv
 import simplifiedSTDP as stdp
 
 
-def neg_sum_func(x):
-    return -(x[0] + x[1])
-
-
-def sum_func(x):
-    return (x[0] + x[1])
-
-
-
-
 def plot_decoded(t, data, xlim_tuple=None):
     if xlim_tuple:
         x_start, x_end = xlim_tuple
@@ -30,10 +20,18 @@ def plot_decoded(t, data, xlim_tuple=None):
     plt.show()
 
 
-def error_func(desired_L, desired_R, actual_L, actual_R):
-    left = np.abs(desired_L - actual_L)
-    right = np.abs(desired_R - actual_R)
-    return np.mean(left + right)
+def error_func(desired_L, desired_R, actual_L, actual_R, window_duration):
+    L = np.array(actual_L)
+    R = np.array(actual_R)
+    L = np.reshape(L, (len(desired_L),-1))
+    R = np.reshape(L, (len(desired_R), -1))
+
+    L_freq = np.count_nonzero(L,axis = 1)/window_duration
+    R_freq = np.count_nonzero(R, axis=1) / window_duration
+
+    left = np.abs(desired_L - L_freq)
+    right = np.abs(desired_R - R_freq)
+    return np.mean((left + right)/2)
 
 
 def read_data():
@@ -43,7 +41,8 @@ def read_data():
     target_L = []
     target_R = []
 
-    with open('C:/Users/Zizi/Desktop/master/Neuromorphic computing/project/NeuroCompProj/SNN/data.csv', newline='') as csvfile:
+    with open('C:/Users/Zizi/Desktop/master/Neuromorphic computing/project/NeuroCompProj/SNN/data.csv',
+              newline='') as csvfile:
         datafile = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in datafile:
             sample_left = []
@@ -60,6 +59,7 @@ def read_data():
 
     return left, right, target_L, target_R
 
+
 def transform_to_train(model, pre_neuron, post_neuron):
     with model:
         Dij = 0.001
@@ -70,13 +70,14 @@ def transform_to_train(model, pre_neuron, post_neuron):
         for conn2 in to_remove:
             model.connections.remove(conn2)
 
-        pre_train_sig_conn = nengo.Connection(train_signal_generator, pre_neuron, synapse = 0.005)
-        post_train_sig_conn = nengo.Connection(train_signal_generator, post_neuron, synapse = 0.005+Dij)
+        nengo.Connection(train_signal_generator, pre_neuron, synapse=0.005)
+        nengo.Connection(train_signal_generator, post_neuron, synapse=0.005 + Dij)
     return model
+
 
 def transform_to_validate(model):
     with model:
-        #remove all trainging connections to input collectors
+        # remove all trainging connections to input collectors
         to_remove = []
         for conn in model.all_connections:
             if conn.pre_obj is inp_collector_lgoal or conn.pre_obj is inp_collector_lter or conn.pre_obj is inp_collector_rgoal or conn.pre_obj is inp_collector_rter:
@@ -84,25 +85,28 @@ def transform_to_validate(model):
         for conn2 in to_remove:
             model.connections.remove(conn2)
 
-        #add the sensor connections back to where they belong
+        # add the sensor connections back to where they belong
         nengo.Connection(inp_collector_lter, input_a)
         nengo.Connection(inp_collector_rter, input_b)
         nengo.Connection(inp_collector_lgoal, input_c)
         nengo.Connection(inp_collector_rgoal, input_d)
     return model
+
+
 with nengo.Network(label="STDP") as model:
-    timing = 0.1
+    timing = 0.060
 
     # train input, initially not connected
     train_signal_generator = nengo.Node(nengo.processes.PresentInput([[0.], [1.]], 0.01))
 
-
-    #sensory input
-    my_spikes_L, my_spikes_R, target_freq_L, target_freq_R = read_data()
-    # my_spikes_L = [[0,0],[0,0]]
+    # sensory input
+    # my_spikes_L, my_spikes_R, target_freq_L, target_freq_R = read_data()
+    my_spikes_L = [[0,0.5],[1.0,0.0],[0.5,0.5],[1.0,1.0],[0.,0]]
+    my_spikes_R = [[0, 0.5], [1.0, 1.0], [0., 0], [1.0, 0.0], [0.5, 0.5]]
+    target_freq_L = [20 , 1, 30, 13 , 12 ]
+    target_freq_R = [ 34, 15, 10 ,2, 30]
     process_L = nengo.processes.PresentInput(my_spikes_L, timing)
     input_node_L = nengo.Node(process_L)
-    # my_spikes_R = [[0], [1]]
     process_R = nengo.processes.PresentInput(my_spikes_R, timing)
     input_node_R = nengo.Node(process_R)
     # https://forum.nengo.ai/t/spike-train-input-to-a-snn-model/717/4
@@ -179,7 +183,6 @@ with nengo.Network(label="STDP") as model:
     nengo.Connection(hidden_c, output_b, solver=nengo.solvers.LstsqL2(weights=True),
                      learning_rule_type=stdp.STDP(learning_rate=2e-9))
 
-
     nengo.Connection(hidden_a, hidden_b)
     nengo.Connection(hidden_b, hidden_a)
     nengo.Connection(hidden_b, hidden_c)
@@ -187,23 +190,25 @@ with nengo.Network(label="STDP") as model:
     nengo.Connection(hidden_c, hidden_a)
     nengo.Connection(hidden_a, hidden_c)
 
-
-duration = 0.1
+duration = 1.3
 error = 10
-error_limit  = 1
+error_limit = 0.5
 pre_neurons = [input_a]
 post_neurons = [input_c]
 index = 0
+
 while error > error_limit:
     with nengo.Simulator(model) as sim:
         sim.data.reset()
-        sim.run(duration)
-    #compute error by comparing the output to the target
+        sim.run(0.3)
+
+    # compute error by comparing the output to the target
     # note: we might want to think about what exactly the output represents
     # and how it relates to the target freqs
     # optionnaly, the output should be transformed somehow
-    new_error = error_func(target_freq_L, target_freq_R, sim.data[outa_p], sim.data[outb_p])#
-    print(error)
+    new_error = error_func(target_freq_L, target_freq_R, sim.data[outa_p], sim.data[outb_p],  0.060)  #
+    print(new_error)
+
     sim.data.reset()
 
     if new_error <= error:
@@ -222,20 +227,10 @@ while error > error_limit:
 
     error = new_error
     model = transform_to_validate(model)
-    index +=1
 
-with nengo.Simulator(model) as sim:
-    sim.run(duration)
-    freq_a = np.sum(sim.data[outa_p] > 0, axis=0) / len(sim.data[outa_p])
-    freq_b = np.sum(sim.data[outb_p] > 0, axis=0) / len(sim.data[outb_p])
-    index +=1
-
+print("final error was", error)
 
 t = sim.trange()
-print("freq A", freq_a)
-print("freq B", freq_b)
-print(sim.data[outa_p])
-print(sim.data[outb_p])
 plot_decoded(t, sim.data)
 
 # Gestolen van tutorial
